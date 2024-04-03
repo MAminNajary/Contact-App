65d76766c208bc390be59a01133e59d5f8b8cc39
M.Amin Najary <mohammadaminnajary@gmail.com>
2024-04-03 14:37:31 +0330

    Add => Header Styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
+import styles from '../styles/Header.module.css';
+
 const Header = () => {
   return (
-    <div>
+    <div className={styles.container}>
         <h1>Contact App</h1>
         <p><a href="https://github.com/MAminNajary">GitHub</a> | React App</p>
     </div>
