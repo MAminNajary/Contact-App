e91ff140296232c899785d9edddaf13286e31661
M.Amin Najary <mohammadaminnajary@gmail.com>
2024-04-03 14:28:58 +0330

    Add => Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div>
+        <h1>Contact App</h1>
+        <p><a href="https://github.com/MAminNajary">GitHub</a> | React App</p>
+    </div>
+  )
+}
+
+export default Header
